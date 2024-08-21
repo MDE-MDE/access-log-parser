75f695c99bacb4f7c395f0f09cb9427d05186d82
Dmitry Minlin <mde92@bk.ru>
2024-08-21 21:50:19 +0500

    Задание по теме "Синтаксис языка" и "Базовые типы данных"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число и нажмите <Enter>:  ");
+        int firstNumb = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число и нажмите <Enter>:  ");
+        int secondNumb = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: " + (firstNumb + secondNumb));
+        System.out.println("Разность чисел: " + (firstNumb - secondNumb));
+        System.out.println("Произведение чисел: " + (firstNumb * secondNumb));
+        if (secondNumb != 0) {
+            System.out.println("Частное чисел: " + (double)(firstNumb / secondNumb));
+        } else {
+            System.out.println("Деление на ноль невозможно!");
+        }
     }
 }
