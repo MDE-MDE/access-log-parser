43712da36263017818c53e47a904cb26fc21628a
Dmitry Minlin <mde92@bk.ru>
2024-09-02 23:32:30 +0500

    Задание по теме "Циклы". Запрашивание пути к файлу в консоли

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File fIle = new File(path);
+            boolean fileExists = fIle.exists();
+            boolean isDirectory = fIle.isDirectory();
+            if (!fileExists || isDirectory) System.out.println("Указанный файл не существует или " +
+                    "указанный путь является путём к папке, а не к файлу");
+            else{
+                count++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + count);
+            }
+        }
+
     }
 }
